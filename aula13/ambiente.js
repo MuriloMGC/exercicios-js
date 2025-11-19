61768c6fb175c98fa64509f6965e90e7ce2c6325
MuriloMGC <58149634+MuriloMGC@users.noreply.github.com>
2025-11-19 18:30:24 -0400

    criado exercicio de laço de repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,18 @@
+var cont = 1
+do{
+    console.log(`Passo ${cont}`)
+    cont++
+}while (cont <=10)
+
+/*var cont = 1
+while (cont <= 3){
+    console.log(`Passo ${cont}`)
+    cont++
+}*/
+
+/*console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')*/
